eed3e8e14e62755e11140cb80547d163336d4fc1
Henry Zhang <sunnywalden@gmail.com>
2019-11-28 22:41:04 +0800

    fix repo addr bug

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Logging ecs automatic via jumper server",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/sunnywalden/login_jump",
+    url="https://github.com/sunnywalden/login_jumper",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
